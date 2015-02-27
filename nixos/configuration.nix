25a857746d2e027c01b70c4489492ff033021ce8
Chih-Kang Huang <asddsa155@gmail.com>
2015-02-27 02:12:51 +0100

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -69,6 +69,12 @@ with lib;
 	windowManager.default = "xmonad";
 	windowManager.xmonad.enable = true;
 	windowManager.xmonad.enableContribAndExtras = true;
+	
+	##### Touchpad #####
+	synaptics = {
+		enable = true;
+		twoFingerScroll = true;
+	};
   };
 
 
