7210203dc36e1304c2719d95dea3aacbe3530afa
Chih-Kang Huang <asddsa155@gmail.com>
2015-02-28 01:34:01 +0100

    Update and rename nixos to nixos/.xmonad/xmonad.hs

---
+++ nixos/.xmonad/xmonad.hs
@@ -0,0 +1,39 @@
+-- xmonad configuration
+-- Chih-Kang Huang
+-- 28/02/2015
+
+-- import modules
+import XMonad  
+import XMonad.Config.Azerty  
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run
+import System.IO
+
+-- Define variables
+myWorkspaces= ["1: Euler","2: Lagrange","3: Poincaré","4: Kantorovich"]
+myTerminal = "urxvt"
+myModMask = mod1Mask -- Alt key
+-- my ModMask = mod4Mask -- Win key
+myBorderWidth = 2
+-- Define workspaces for certain applications
+myManageHook= composeAll
+	[ className =? "Firefox" --> doShift "1: Euler"
+	]
+
+main = do
+	xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmobarrc"
+	xmonad $ azertyConfig  
+		{ workspaces = myWorkspaces
+		, modMask = myModMask
+		, terminal = myTerminal
+		, borderWidth = myBorderWidth
+		, manageHook = myManageHook <+> manageHook defaultConfig
+		, layoutHook = avoidStruts $ layoutHook defaultConfig
+  		, startupHook = startup
+		}
+
+-- startup applications
+startup :: X()
+startup = do 
+	spawn "exec --no-startup-id feh --bg-scale ~/Images/math_pictures/T-Gyroid.jpg"
